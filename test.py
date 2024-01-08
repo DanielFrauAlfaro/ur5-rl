5aa5ab8106b97440d8c3bb7d3d8dcc4453af02f9
Daniel Frau Alfaro <danielfraualfa@gmail.com>
2024-01-08 20:06:55 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import ur5_rl
 import gymnasium as gym
 
 print("|| Compiling ...")
-env = gym.make("ur5_rl/Ur5Env-v0", render_mode = "DIRECT")
+env = gym.make("ur5_rl/Ur5Env-v0", render_mode = "GUI")
 env_ = gym.make_vec("ur5_rl/Ur5Env-v0", num_envs=3)
 print("|| Reseting environment ...")
 
@@ -11,7 +11,7 @@ print("|| Sampling ...")
 for j in range(3):
     print("--- Epoch ", j)
     obs, __ = env.reset(seed=0, options={})
-    
+
     while True:
 
         # TODO: función del agente
@@ -22,7 +22,7 @@ for j in range(3):
         env.render()
         
         # if truncated:
-        #     obs, __ = env.reset(seed=0, options={})
+        #     break
         
         # TODO: actualizar estado del agente --> después o durante el episodio
 
