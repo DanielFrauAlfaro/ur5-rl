a98500d10c516c4ff3cf5896412bed8da54f4886
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2024-01-24 19:00:18 +0100

    Trainings and reparametrisation

--- test.py
+++ test.py
@@ -3,8 +3,9 @@ import gymnasium as gym
 from stable_baselines3 import SAC
 from networks_SB import CustomCombinedExtractor
 import numpy as np
+import cv2 as cv
 
-
+TEST = True
 
 if __name__ == "__main__":
     print("|| Compiling ...")
@@ -17,13 +18,13 @@ if __name__ == "__main__":
     q_shape = q_space.shape
     in_channels, frame_w, frame_h = image_space.shape
     
-    residual = True
-    channels = [in_channels, 16, 32, 32]
+    residual = False
+    channels = [in_channels, 16, 32]
     kernel = 3          
-    m_kernel = 5
+    m_kernel = 3
     n_layers = len(channels) - 1
 
-    out_vector_features = 50
+    out_vector_features = 70
     features_dim = 128          
 
     # Use your custom feature extractor in the policy_kwargs
@@ -34,20 +35,27 @@ if __name__ == "__main__":
                                        channels = channels, kernel = kernel, m_kernel = m_kernel,
                                        n_layers = n_layers, out_vector_features = out_vector_features),
         net_arch=dict(
-            pi=[features_dim, 32],  # Adjust the size of these layers based on your requirements
-            vf=[features_dim, 32],  # Adjust the size of these layers based on your requirements
-            qf=[features_dim, 32]),
+            pi=[features_dim, 16],  # Adjust the size of these layers based on your requirements
+            vf=[features_dim, 16],  # Adjust the size of these layers based on your requirements
+            qf=[features_dim, 16]),
         share_features_extractor = True
     )
 
     model = SAC("MultiInputPolicy", env, policy_kwargs=policy_kwargs, 
-                verbose=100, buffer_size = 30000,  batch_size = 256, tensorboard_log="logs/", train_freq=10,
-                learning_rate = 0.00073, gamma = 0.98, seed = 42,
-                use_sde = True, sde_sample_freq = 8)         # See logs: tensorboard --logdir logs/
-    # print(model.policy)
-    # model.learn(total_timesteps=100000, log_interval=5, tb_log_name= "Test", progress_bar = True)
+                verbose=100, buffer_size = 10000,  batch_size = 128, tensorboard_log="logs/", train_freq=10,
+                learning_rate = 0.001, gamma = 0.99, seed = 42,
+                use_sde = False, sde_sample_freq = 8)         # See logs: tensorboard --logdir logs/
     
-    # model.save("./models/sac_ur5_stage_1.0")
+
+    if not TEST:
+        model.learn(total_timesteps=6300, log_interval=5, tb_log_name= "Test", progress_bar = True)
+        model.save("./models/sac_ur5_stage_teset")
+    else:
+        model = SAC.load("./models/sac_ur5_stage_teset")
+    
+    
+    model.policy.eval
+    print("... Testing ...")
     
     # Testing
     obs, info = env.reset()
@@ -56,8 +64,6 @@ if __name__ == "__main__":
         # action = env.action_space.sample()
 
         obs, reward, terminated, truncated, info = env.step(action)
-
-        # env.render()
 
         if terminated or truncated:
             obs, info = env.reset()
