70ca5309c0d831a38f99f4ddaf7d24eab7eeb3d6
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2024-01-24 19:32:49 +0100

    Multiple environments training

--- test.py
+++ test.py
@@ -1,16 +1,48 @@
 import ur5_rl
 import gymnasium as gym
 from stable_baselines3 import SAC
+from stable_baselines3 import SAC
+from stable_baselines3.common.vec_env import SubprocVecEnv
 from networks_SB import CustomCombinedExtractor
 import numpy as np
 import cv2 as cv
 
-TEST = True
+
+TEST = False
+
+def make_env(env_id: str, rank: int, seed: int = 0):
+    """
+    Utility function for multiprocessed env.
+
+    :param env_id: the environment ID
+    :param num_env: the number of environments you wish to have in subprocesses
+    :param seed: the inital seed for RNG
+    :param rank: index of the subprocess
+    """
+    def _init():
+        env = gym.make(env_id, render_mode="DIRECT")
+        env.reset(seed=seed + rank)
+        return env
+
+    return _init
+
+
 
 if __name__ == "__main__":
     print("|| Compiling ...")
-    env = gym.make("ur5_rl/Ur5Env-v0", render_mode = "GUI")
+    env = gym.make("ur5_rl/Ur5Env-v0", render_mode = "DIRECT")
     
+
+
+
+
+    env_id = "ur5_rl/Ur5Env-v0"
+    num_cpu = 3  # Number of processes to use
+    # Create the vectorized environment
+    vec_env = SubprocVecEnv([make_env(env_id, i) for i in range(num_cpu)])
+
+
+
 
     q_space = env.observation_space["ee_position"]
     image_space = env.observation_space["image"]
@@ -41,7 +73,7 @@ if __name__ == "__main__":
         share_features_extractor = True
     )
 
-    model = SAC("MultiInputPolicy", env, policy_kwargs=policy_kwargs, 
+    model = SAC("MultiInputPolicy", vec_env, policy_kwargs=policy_kwargs, 
                 verbose=100, buffer_size = 10000,  batch_size = 128, tensorboard_log="logs/", train_freq=10,
                 learning_rate = 0.001, gamma = 0.99, seed = 42,
                 use_sde = False, sde_sample_freq = 8)         # See logs: tensorboard --logdir logs/
@@ -51,23 +83,23 @@ if __name__ == "__main__":
         model.learn(total_timesteps=6300, log_interval=5, tb_log_name= "Test", progress_bar = True)
         model.save("./models/sac_ur5_stage_teset")
     else:
-        model = SAC.load("./models/sac_ur5_stage_teset")
+        model = SAC.load("./models/sac_ur5_stage_1.2")
     
     
-    model.policy.eval
+    model.policy.eval()
     print("... Testing ...")
     
-    # Testing
     obs, info = env.reset()
     while True:
         action, _states = model.predict(obs)
-        # action = env.action_space.sample()
-
         obs, reward, terminated, truncated, info = env.step(action)
 
         if terminated or truncated:
             obs, info = env.reset()
+            
 
+    
+    
 
     # print("|| Sampling ...")
     # for j in range(100):
