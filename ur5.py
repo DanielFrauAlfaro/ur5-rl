483f609eaaa8e3613e5a62e1c8b449e2e9b52cb8
Daniel Frau Alfaro <danielfraualfa@gmail.com>
2024-01-30 19:47:49 +0100

    Interface with incremental

--- ur5.py
+++ ur5.py
@@ -9,6 +9,7 @@ from spatialmath import SE3
 import roboticstoolbox as rtb
 from math import pi
 import os
+import time
 
 ur5 = rtb.DHRobot([
             rtb.RevoluteDH(d=0.1625, alpha=pi/2.0),
@@ -39,12 +40,19 @@ def user_interface(): # [0.1332997  0.49190053 0.48789219]    //      [-3.141575
 
     # gripper_palm = p.addUserDebugParameter('Palm', 1, 0, 1)
 
-    x_gui = p.addUserDebugParameter('X', -1.5, 1.5, 0.1332997)
-    y_gui = p.addUserDebugParameter('Y', -1.5, 1.5, 0.49190053)
-    z_gui = p.addUserDebugParameter('Z', -1.5, 1.5, 0.48789219)
-    roll_gui = p.addUserDebugParameter('Roll', -5, 0.0, -3.14)
-    pitch_gui = p.addUserDebugParameter('Pitch', -1.5, 1.5, 0)
-    yaw_gui = p.addUserDebugParameter('Yaw', -3.1415, 0.0, -2.35658978)
+    x_gui = p.addUserDebugParameter('X', -1, 1, 0.0)
+    y_gui = p.addUserDebugParameter('Y', -1, 1, 0.0)
+    z_gui = p.addUserDebugParameter('Z', -1, 1, 0.0)
+    roll_gui = p.addUserDebugParameter('Roll', -1, 1, 0)
+    pitch_gui = p.addUserDebugParameter('Pitch', -1, 1, 0)
+    yaw_gui = p.addUserDebugParameter('Yaw', -1, 1, 0)
+
+    # x_gui = p.addUserDebugParameter('X', -1.5, 1.5, 0.1332997)
+    # y_gui = p.addUserDebugParameter('Y', -1.5, 1.5, 0.49190053)
+    # z_gui = p.addUserDebugParameter('Z', -1.5, 1.5, 0.48789219)
+    # roll_gui = p.addUserDebugParameter('Roll', 0.0, 2, 2)
+    # pitch_gui = p.addUserDebugParameter('Pitch', -1.5, 1.5, 0)
+    # yaw_gui = p.addUserDebugParameter('Yaw', -5, 0.0, -3)
 
     # return [shoulder_pan_gui, shoulder_lift_gui, elbow_gui, wrist_1_gui, wrist_2_gui, wrist_3_gui, gripper_1, gripper_2, gripper_mid, gripper_palm,
     return [x_gui, y_gui, z_gui, roll_gui, pitch_gui, yaw_gui]
@@ -98,29 +106,24 @@ def compute_ik(robot_id, action, joints, q):
     roll = action[3]
     pitch = action[4]
     yaw = action[5]
-
+    print(action)
     # Builds up homogeneus matrix
     T = SE3(x, y, z)
     T_ = SE3.RPY(roll, pitch, yaw, order='zyx')
 
     T = T * T_
 
+    print(T.t)
+    print(T.rpy('zyx'))
+
     # Computes inverse kinematics
     new_q = ur5.ik_LM(T,q0 = q)
 
-    # print(q)
-    # print("Roll: ", )
-    # print("Pitch: ",[0][4])
-    # print("Yaw: ", new_q[0][5])
+    print("New Q: ", new_q[0])
+    print("\n\n")
+    # raise
+    
 
-    # if new_q[0][3] < -pi:
-    #     new_q[0][3] = 
-
-    # print("\n") 
-
-    # print(len(joints))
-    # print(len(new_q[0]))
-    # print("\n")
     p.setJointMotorControlArray(bodyUniqueId=robot_id, 
                                     jointIndices=joints, 
                                     controlMode=p.POSITION_CONTROL,
@@ -137,8 +140,31 @@ def spawn_environment(id):
                               basePosition=[0, 0, 0], 
                               physicsClientId=client, 
                               useFixedBase=1)
-    return ur5_id
+    
+    euler = [-1.57,0,-1.57]
+    orientation = p.getQuaternionFromEuler(euler, physicsClientId=client)
+    table = p.loadURDF(fileName=os.path.dirname(__file__) + '/ur5_rl/resources/models/models/table.urdf',
+                              basePosition=[-0.16, 1.0, 0.74], 
+                              baseOrientation=orientation, 
+                              physicsClientId=client, 
+                              useFixedBase=1)
+    
+    # List of object directories
+    spawnables = ("002_master_chef_can", "003_cracker_box", "004_sugar_box", "005_tomato_soup_can", "006_mustard_bottle", "007_tuna_fish_can", "008_pudding_box", "009_gelatin_box", "010_potted_meat_can", "011_banana", "012_strawberry", "013_apple", "014_lemon", "015_peach", "016_pear", "017_orange", "018_plum", "021_bleach_cleanser")
 
+    # Random object position and orientation
+    pos, orn = np.random.uniform([[0.01, 0.45, 0.85], [-3.1415,-3.1415, -3.1415]], 
+                                    [[0.3,  0.65, 0.85], [3.1415,  3.1415,  3.1415]])
+
+    rand_orientation = p.getQuaternionFromEuler(orn)
+
+    # Load URDF model of selected object: pybullet_URDF_objects/' + spawnables[object] + '/model.urdf',
+    object = p.loadURDF(fileName=os.path.dirname(__file__) + '/ur5_rl/resources/models/pybullet_URDF_objects/' + spawnables[0] + '/model.urdf',
+                                basePosition=pos,
+                                baseOrientation=rand_orientation, 
+                                physicsClientId=client)
+    
+    return ur5_id
 
 # Process all model joints: obtain data of each joint
 def setup_robot(robotID):
@@ -179,8 +205,8 @@ def setup_robot(robotID):
         
         list_attr[jointName] = jointID
 
-        print("Name: ", jointName, "Joint Index:", i, "Link Index:", info[12])
-        print("--")
+        # print("Name: ", jointName, "Joint Index:", i, "Link Index:", info[12])
+        # print("--")
 
 
         # If a joint is controllable ...
@@ -219,10 +245,6 @@ def setup_robot(robotID):
 
 # Main
 if __name__ == "__main__":
-    
-    
-
-
     client = p.connect(p.GUI)
 
     gui_joints = user_interface()
@@ -233,67 +255,72 @@ if __name__ == "__main__":
 
     joints, __ = setup_robot(ur5_id)
 
-    # mimic_finger_1 = gripperControl(ur5_id, joints, '1')
-    # mimic_finger_2 = gripperControl(ur5_id, joints, '2')
-    # mimic_finger_middle = gripperControl(ur5_id, joints, 'middle')
-
-    # mimic_palm = palmControl(ur5_id, joints)
 
     palm = 0
     prev_palm = 0
     state_palm = 0
 
-    set_joints(ur5_id, [0.0, -1.5708, -1.5708, -1.5708, 1.5708, -0.785 + pi])
+    q = [0.0, -1.5708, -1.5708, -1.5708, 1.5708, 2.3]
 
-    for __ in range(25):
+    set_joints(ur5_id, q)
+    
+
+
+    ee = [0.1332997, 0.49, 0.48, -3.14, 0.0, -2.3]
+
+    for __ in range(40):
         p.stepSimulation()
+
+    T = ur5.fkine(q, order='yxz')
+    ee_pos = T.t
+    ee_or = T.rpy('yxz')
+
+    ee = [ee_pos[0], ee_pos[1], ee_pos[2], ee_or[0], ee_or[1], ee_or[2]]
+
+
+    print("Init ee_pos: ", ee_pos)
+    print("Init ee_or: ", ee_or)
+    print("\n\n")
+    
     
     while True:
         
         j = read_gui(gui_joints)
-        # palm = j[-1]
-
-        # if palm != prev_palm:
-        #     state_palm = state_palm + 1
-
-        #     if state_palm > 2:
-        #         state_palm = 0
-
-        # if state_palm == 0:
-        #     j[-1] = -0.19
-        # elif state_palm == 1:
-        #     j[-1] = 0
-        # else:
-        #     j[-1] = 0.17
-
-        
-        # prev_palm = palm
-
-        q = []
-
-        # # UR5 joint values
-        for i in joints:
-            aux = p.getJointState(bodyUniqueId=ur5_id, 
-                                jointIndex=i,
-                                physicsClientId=client)
-
-            q.append(aux[0])
+        j = (np.array(j) + np.array(ee)).tolist()
+        # j = ee
 
         # set_joints(ur5_id, j)
         compute_ik(ur5_id, j, joints, q)
 
+        p.stepSimulation()
+
+        # # UR5 joint values
+        for idx, i in enumerate(joints):
+            aux = p.getJointState(bodyUniqueId=ur5_id, 
+                                jointIndex=i,
+                                physicsClientId=client)
+
+            q[idx] = aux[0]
+
         # render(client, ur5_id)
-        T = ur5.fkine(q, order='zyx')
-        state = p.getLinkState(bodyUniqueId = ur5_id, linkIndex = 11, computeForwardKinematics = True)
+        T = ur5.fkine(q, order='yxz')
+        ee_pos = T.t
+        ee_or = T.rpy('yxz')
+        ee = [ee_pos[0], ee_pos[1], ee_pos[2], ee_or[0], ee_or[1], ee_or[2]]
 
-
+        # state = p.getLinkState(bodyUniqueId = ur5_id, linkIndex = 11, computeForwardKinematics = True)
 
         print("Robotic Toolboox T: ", T.t)
-        print("Robotic Toolbox R: ", T.rpy(order='zyx'))
-        print("--")
+        print("Robotic Toolbox R: ", T.rpy('yxz'))
+        # print(q)
+        print("--\n\n")
+
+        time.sleep(0.1)
+        
+        
         
         # print("Pybullet API T: ", state[0])
         # print("Pybullet aPI R: ", p.getEulerFromQuaternion(state[1]))
         # print("--\n\n")
 
-        p.stepSimulation()
+        
