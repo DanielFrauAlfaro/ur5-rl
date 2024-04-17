Quaternion multiplication fixed with Numpy and Pytorch

ur5.py
@@ -300,11 +300,13 @@ def rotation_matrix_to_euler_xyz(R):
 
 
 
+
 def get_quaternion(q):
     return np.array([q[-1], q[0], q[1], q[2]])
 
 def get_dualQuaternion(q_r, q_t):
-    return np.concatenate((q_r, 0.5*q_t*q_r))
+    return np.concatenate((q_r, 0.5*q_mul(torch.tensor([q_t]), torch.tensor([q_r])).numpy()[0]))
+
 
 # Main
 if __name__ == "__main__":