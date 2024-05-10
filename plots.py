0cb0d7752b331d4a56025f1e998c5476d3b22d50
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2024-05-10 14:40:58 +0200

    Plots with index, not name

--- plots.py
+++ plots.py
@@ -8,8 +8,14 @@ with open('test_results_withError.json', 'r') as file:
 # Sort data by index
 sorted_data = sorted(json_data.items(), key=lambda x: x[1]['idx'])
 
+# for elem in sorted_data:
+#     print(elem[0])
+#     print(type(elem))
+#     elem[0] = "AAA"
+#     raise
+
 # Extract sorted keys and mean_reward values
-sorted_keys = [item[0] for item in sorted_data]
+sorted_keys = [item[1]["idx"] for item in sorted_data]
 mean_rewards = [item[1]['mean_reward'] for item in sorted_data]
 d_error = [item[1]['distance_error'] for item in sorted_data]
 or_error = [item[1]['orientation_error'] for item in sorted_data]
@@ -18,40 +24,40 @@ dq_error = [item[1]['dq_error'] for item in sorted_data]
 # Plot mean_reward values vs sorted keys as a line plot with background grid
 plt.figure(figsize=(10, 6))
 plt.plot(sorted_keys, mean_rewards, marker='o')  # Use marker='o' for data points
-plt.xlabel('Model')
+plt.xlabel('Steps')
 plt.ylabel('Mean Reward')
 plt.xticks(rotation=90)  # Rotate x-axis labels for better visibility
-plt.title('Mean Reward (Ordered by Index)')
+plt.title('')
 plt.grid(True)  # Add background grid
 plt.tight_layout()
 plt.show()
 
 plt.figure(figsize=(10, 6))
 plt.plot(sorted_keys, d_error, marker='o')  # Use marker='o' for data points
-plt.xlabel('Model')
+plt.xlabel('Steps')
 plt.ylabel('Distance error (m)')
 plt.xticks(rotation=90)  # Rotate x-axis labels for better visibility
-plt.title('Distance error (Ordered by Index)')
+plt.title('')
 plt.grid(True)  # Add background grid
 plt.tight_layout()
 plt.show()
 
 plt.figure(figsize=(10, 6))
 plt.plot(sorted_keys, or_error, marker='o')  # Use marker='o' for data points
-plt.xlabel('Model')
-plt.ylabel('Orientation error (º)')
+plt.xlabel('Steps')
+plt.ylabel('Orientation error (rad)')
 plt.xticks(rotation=90)  # Rotate x-axis labels for better visibility
-plt.title('Orientation Error (Ordered by Index)')
+plt.title('')
 plt.grid(True)  # Add background grid
 plt.tight_layout()
 plt.show()
 
 plt.figure(figsize=(10, 6))
 plt.plot(sorted_keys, dq_error, marker='o')  # Use marker='o' for data points
-plt.xlabel('Model')
+plt.xlabel('Steps')
 plt.ylabel('DQ error')
 plt.xticks(rotation=90)  # Rotate x-axis labels for better visibility
-plt.title('Dual Quaternion Error (Ordered by Index)')
+plt.title('Dual ')
 plt.grid(True)  # Add background grid
 plt.tight_layout()
 plt.show()
