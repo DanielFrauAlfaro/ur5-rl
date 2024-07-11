730483140e0332310868f721d16a9f7acd530ff9
Daniel Frau Alfaro <98766327+DanielFrauAlfaro@users.noreply.github.com>
2024-07-11 14:29:48 +0200

    Parser argument manager

--- my_parser.py
+++ my_parser.py
@@ -1,7 +1,21 @@
 import yaml
 
-with open('./config/config.yml', 'r') as f:
-    data = yaml.load(f, Loader=yaml.SafeLoader)
+class Param_manager():
+        
+    def get_data(self):
+        return self.data
+    
+    def set_data(self, path):
+        with open(path, 'r') as f:
+            self.data = yaml.load(f, Loader=yaml.SafeLoader)
 
-# Print the values as a dictionary
-print(data)
+        # with open(self.data["sim_conf"], "r") as s, \
+        #      open(self.data["agent_conf"], "r") as a, \
+        #      open(self.data["model_conf"], "r") as m:
+            
+        #     file_list = [s, a, m]
+            
+        #     for i in file_list:
+        #         self.data.update(yaml.load(i, Loader=yaml.SafeLoader))
+            
+            
